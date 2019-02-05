d7a3e378eec871a61281036116209d7b384d2267
cedwards14 <35229175+cedwards14@users.noreply.github.com>
2019-02-04 18:10:52 -0700

    Updated my script
    got rid of some code that wasnt being  used. Added a new line into the script to help with readability for the text doc.  Added some comments

--- kml_to_insert_builder/kml_insert_builder.py
+++ kml_to_insert_builder/kml_insert_builder.py
@@ -2,28 +2,33 @@
 from fastkml import kml
 from bs4 import BeautifulSoup
 
-
-k = kml.KML()
-kml_file = "BlueTrail.kml"
-with open(kml_file, 'rt')as myfile:
-    doc = myfile.read().encode('utf-8')
-
-k.from_string(doc)
-features = list(k.features())
-soup = BeautifulSoup(doc, "xml")
-
 id_lat_long = []
 my_latlongs = []
 my_lat = []
 my_long = []
 
 
+kml_file = "BlueTrail.kml"
+
+with open(kml_file, 'rt')as myfile:
+    doc = myfile.read().encode('utf-8')
+
+
+soup = BeautifulSoup(doc, "xml")
+
+
+
+
 for coord in soup.find_all('Placemark'):
     for coords in coord.find_all('coordinates'):
+        # coordinates coems with a ,0 at the end so i want to remove it
         my_latlongs = coords.text.replace(',0', '').split(",")
+        # removes the empty space
     my_lat.append(my_latlongs[1].split())
     my_long.append(my_latlongs[0].split())
 
+
+# put my id, lat and long into a new list
 i = 11
 while i < len(my_lat):
     value = [i+1] + my_lat[i] + my_long[i]
@@ -31,10 +36,10 @@ while i < len(my_lat):
     i += 1
 
 
-my_insert = []
+
 file = open("latLong.txt", "w")
 for item in id_lat_long:
-    value = """insert into Polylines(polylineId, cityId, markerId, trailId, lat, long) values({0}, 1, 40, 2, {1}, {2});""".format(item[0], item[1], item[2])
+    value = """\n insert into Polylines(polylineId, cityId, markerId, trailId, lat, long) values({0}, 1, 40, 2, {1}, {2});""".format(item[0], item[1], item[2])
     file.write(value)    
 
-print(my_insert)
+print(file)
