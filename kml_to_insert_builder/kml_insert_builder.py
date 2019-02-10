318be99fac760f1894d03ef34794e1a141f9c556
cedwards14 <35229175+cedwards14@users.noreply.github.com>
2019-02-10 11:27:49 -0700

    updated the script to add the correct trail and associated marker number

--- kml_to_insert_builder/kml_insert_builder.py
+++ kml_to_insert_builder/kml_insert_builder.py
@@ -5,19 +5,26 @@ id_lat_long = []
 my_latlongs = []
 my_lat = []
 my_long = []
+my_markers = []
 
-i = 10
-kml_file = "BlueTrail.kml"
+# marker_id = 1
+# trail_id = 1
+# kml_file = "greenTrail.kml"
+
+# marker_id = 320
+# trail_id = 2
+# kml_file = "blueTrail.kml"
 
 
-# i = 154
-# kml_file = "GreenTrail.kml"
+# marker_id = 480
+# trail_id = 3
+# kml_file = "redTrail.kml"
 
-# i = 250
-# kml_file = "OrangeTrail.kml"
 
-# i = 350
-# kml_file = "RedTrail.kml"
+marker_id = 580
+trail_id = 4
+kml_file = "orangeTrail.kml"
+
 
 with open(kml_file, 'rt')as myfile:
     doc = myfile.read().encode('utf-8')
@@ -26,10 +33,9 @@ with open(kml_file, 'rt')as myfile:
 soup = BeautifulSoup(doc, "xml")
 
 
-
-
 for coord in soup.find_all('Placemark'):
-    print(coord)
+    for markers in coord.find_all('description'):
+        my_markers.append(markers.text)
     for coords in coord.find_all('coordinates'):
         # coordinates comes with a ,0 at the end so I want to remove it
         my_latlongs = coords.text.replace(',0', '').split(",")
@@ -38,17 +44,22 @@ for coord in soup.find_all('Placemark'):
     my_long.append(my_latlongs[0].split())
 
 
+i = 0
 # put my id, lat and long into a new list
 while i < len(my_lat):
-    value = [i+1] + my_lat[i] + my_long[i]
+    value = [marker_id] + my_lat[i] + my_long[i]
     id_lat_long.append(value)
+    id_lat_long[i].append(my_markers[i])
+    id_lat_long[i].append(trail_id)
     i += 1
-
+    marker_id += 1
 
 
 file = open("latLong.txt", "w")
 for item in id_lat_long:
-    value = """\n insert into Polylines(polylineId, cityId, markerId, trailId, lat, long) values({0}, 1, 40, 2, {1}, {2});""".format(item[0], item[1], item[2])
-    file.write(value)    
+    value = """\n insert into Polylines(polylineId, cityId, markerId, trailId, lat, long) values({0}, 1, {3}, {4}, {1}, {2});""".format(
+        item[0], item[1], item[2], item[3], item[4])
+    file.write(value)
 
-print(file)
+
+print(len(id_lat_long))
