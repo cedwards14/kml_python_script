ad5b025deca375e1bdf4e5f7470b2c18ded23bbf
cedwards14 <35229175+cedwards14@users.noreply.github.com>
2019-05-01 16:35:10 -0600

    Updated my script to run through all 4 trails and make every marker
    This is way more effecient to work with instead fo running 4 times for each trail individually

--- kml_to_insert_builder/tempCodeRunnerFile.py
+++ kml_to_insert_builder/tempCodeRunnerFile.py
@@ -1,11 +1,11 @@
 
 from bs4 import BeautifulSoup
 
-id_lat_long = []
-my_latlongs = []
-my_lat = []
-my_long = []
-my_markers = []
+# id_lat_long = []
+# my_latlongs = []
+# my_lat = []
+# my_long = []
+# my_markers = []
 
 # marker_id = 1
 # trail_id = 1
@@ -21,47 +21,95 @@ my_markers = []
 # kml_file = "RedTrail.kml"
 
 
-marker_id = 669
-trail_id = 4
-kml_file = "OrangeTrail.kml"
+# marker_id = 669
+# trail_id = 4
+# kml_file = "OrangeTrail.kml"
 
 
-with open(kml_file, 'rt')as myfile:
-    doc = myfile.read().encode('utf-8')
+
+# with open(kml_file, 'rt')as myfile:
+#     doc = myfile.read().encode('utf-8')
 
 
-soup = BeautifulSoup(doc, "xml")
+# soup = BeautifulSoup(doc, "xml")
 
 
-for coord in soup.find_all('Placemark'):
-    for markers in coord.find_all('description'):
-        my_markers.append(markers.text)
-    for coords in coord.find_all('coordinates'):
-        # coordinates comes with a ,0 at the end so I want to remove it
-        my_latlongs = coords.text.replace(',0', '').split(",")
-        # removes the empty space
-    my_lat.append(my_latlongs[1].split())
-    my_long.append(my_latlongs[0].split())
+# for coord in soup.find_all('Placemark'):
+#     for markers in coord.find_all('description'):
+#         my_markers.append(markers.text)
+#     for coords in coord.find_all('coordinates'):
+#         # coordinates comes with a ,0 at the end so I want to remove it
+#         my_latlongs = coords.text.replace(',0', '').split(",")
+#         # removes the empty space
+#     my_lat.append(my_latlongs[1].split())
+#     my_long.append(my_latlongs[0].split())
 
 
-print(len(my_lat))
-print(len(my_markers))
-i = 0
+# print(len(my_lat))
+# print(len(my_markers))
+# i = 0
 # put my id, lat and long into a new list
-while i < len(my_lat):
-    value = [marker_id] + my_lat[i] + my_long[i]
-    id_lat_long.append(value)
-    id_lat_long[i].append(my_markers[i])
-    id_lat_long[i].append(trail_id)
-    i += 1
-    marker_id += 1
+# while i < len(my_lat):
+#     value = [marker_id] + my_lat[i] + my_long[i]
+#     id_lat_long.append(value)
+#     id_lat_long[i].append(my_markers[i])
+#     id_lat_long[i].append(trail_id)
+#     i += 1
+#     marker_id += 1
 
 
+# file = open("latLong.txt", "w")
+# for item in id_lat_long:
+#     value = """\n insert into Polylines(polylineId, cityId, markerId, trailId, lat, long) values({0}, 1, {3}, {4}, {1}, {2});""".format(
+#         item[0], item[1], item[2], item[3], item[4])
+#     file.write(value)
+
+
+# print(len(id_lat_long))
+id_lat_long = []
+my_latlongs = []
+my_lat = []
+my_long = []
+my_markers = []
+complete_list = []
+markerId = 1
+trailId = 1
+i = 0
+
+kml_list = ["GreenTrail.kml", "BlueTrail.kml", "RedTrail.kml", "OrangeTrail.kml" ]
+# kml_list = ["GreenTrail.kml", "BlueTrail.kml" ]
+
+print(kml_list)
+
+for trail in kml_list:
+    with open(trail, 'rt')as myfile:
+        doc = myfile.read().encode('utf-8')
+    soup = BeautifulSoup(doc, "xml")
+    
+    for coord in soup.find_all('Placemark'):
+        for coords in coord.find_all('coordinates'):
+            # coordinates comes with a ,0 at the end so I want to remove it
+            my_latlongs = coords.text.replace(',0', '').split(",")
+            # removes the empty space
+        my_lat.append(my_latlongs[1].split())
+        my_long.append(my_latlongs[0].split())
+
+  
+        # put my id, lat and long into a new list
+    while i < len(my_lat):
+        value = [markerId] + my_lat[i] + my_long[i]
+        id_lat_long.append(value)
+        id_lat_long[i].append(trailId)
+        i += 1
+        markerId += 1    
+    trailId+=1
+print(id_lat_long)
+    # complete_list.append(id_lat_long) 
+       
+    # print(complete_list)
 file = open("latLong.txt", "w")
 for item in id_lat_long:
-    value = """\n insert into Polylines(polylineId, cityId, markerId, trailId, lat, long) values({0}, 1, {3}, {4}, {1}, {2});""".format(
-        item[0], item[1], item[2], item[3], item[4])
+    value = """\n insert into Polylines(polylineId, cityId, trailId, lat, long) values({0}, 1, {3}, {1}, {2});""".format(item[0], item[1], item[2], item[3])
     file.write(value)
-
-
-print(len(id_lat_long))
+    
+         
